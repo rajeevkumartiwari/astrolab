21796580cb268dc79ef9ea9fbf7b50b24e67bf9e
RAJEEV TIWARI <rajeevkumartiwari@gmail.com>
2025-04-12 13:51:22 -0700

    Initial files

---
+++ main.py
@@ -0,0 +1,11 @@
+from logic.distances import get_earth_moon_distance_m
+from logic.plotting import plot_solar_system
+import time
+if __name__ == "__main__":
+    print("Starting program...")
+    # while True:
+    #     distance = get_earth_moon_distance_m()
+    #     print(f"Distance between Earth and Moon: {distance} meters")
+    #     time.sleep(5)
+
+    plot_solar_system()
